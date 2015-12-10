4c745825549f419b7e82a68e0f6a99e50da02fb4
jgillick <j_gillick@yahoo.com>
2015-12-10 00:37:59 -0800

    Spaces specific settings

---
+++ spaces/settings.py
@@ -0,0 +1,14 @@
+
+# The name of the spaces admin group
+ADMIN_GROUP_NAME = "Spaces Admin"
+EDITOR_GROUP_NAME = "Editor"
+EDITOR_GROUP_NAME = "Viewer"
+
+# Reserved root slug names
+RESERVED_ROOT_URLS = [
+    'login',
+    'user',
+    's',
+    'reports',
+    'admin'
+]
--- spaces/urls.py
+++ spaces/urls.py
@@ -1,15 +1,6 @@
 from django.conf.urls import url
 from . import views
 
-RESERVED_ROOT_URLS = [
-    'login',
-    'user',
-    's',
-    'settings',
-    'reports',
-    'admin'
-]
-
 app_name = 'spaces'
 urlpatterns = [
     url(r'^login$', views.LoginView.as_view(), name='login'),
