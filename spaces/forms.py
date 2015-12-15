dee4fc898b60c1e15652299cc4ebd29e46f24fa2
jgillick <j_gillick@yahoo.com>
2015-12-14 23:53:46 -0800

    Use same view class for Create/Edit document

--- spaces/forms.py
+++ spaces/forms.py
@@ -13,12 +13,27 @@ class DocumentForm(forms.ModelForm):
 
 
 class RevisionFormset(BaseInlineFormSet):
-    def set_author(self, author):
-        self.cleaned_data[0]['author'] = author
-        print self.cleaned_data
 
-    def clean(self):
-        super(RevisionFormset, self).clean()
+    def __init__(self, *args, **kwargs):
+        self.author = kwargs.pop('author', None)
+        super(RevisionFormset, self).__init__(*args, **kwargs)
+
+        if self.author is not None:
+            for form in self.forms:
+                if form.instance.pk is None:
+                    form.instance.author = self.author
+
+    def clean(self, *args, **kwargs):
+        super(RevisionFormset, self).clean(*args, **kwargs)
+
+        # Validate the the author is the logged in user
+        # for form in self.forms:
+        #     print form.cleaned_data
+        #     if form.cleaned_data.get('author') != self.author:
+        #         form.add_error(
+        #             'author',
+        #             'There was an error setting the author')
+
 
 RevisionInlineFormset = inlineformset_factory(
     Document,
@@ -26,4 +41,5 @@ RevisionInlineFormset = inlineformset_factory(
     formset=RevisionFormset,
     extra=1, max_num=1,
     fields=('content',),
+    # exclude=(),
     can_delete=False)
