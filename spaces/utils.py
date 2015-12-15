1cd3096322b5d4b4c4df0f1fba6891e29c911c53
jgillick <j_gillick@yahoo.com>
2015-12-14 23:54:36 -0800

    Convert path to lowercase when normalizing

--- spaces/utils.py
+++ spaces/utils.py
@@ -9,25 +9,29 @@ def normalize_path(path):
      * Removes extra and trailing slashes
      * Converts special characters to underscore
     """
+    if path is None:
+        return ""
+
     path = re.sub(r'/+', '/', path)     # repeated slash
     path = re.sub(r'/*$', '', path)     # trailing slash
-    
+
     path = [to_slug(p) for p in path.split(os.sep)]
-    
-    return os.sep.join(path) # preserves leading slash
+
+    return os.sep.join(path)  # preserves leading slash
+
 
 def to_slug(value):
-    """ 
-    Convert a string to a URL slug
-    """
+    """ Convert a string to a URL slug. """
+    value = value.lower()
+
     # Space to dashes
-    value = re.sub(r'[\s_]+', '-', value) 
+    value = re.sub(r'[\s_]+', '-', value)
 
     # Special characters
     value = re.sub(r'[^a-z0-9\-]+', '', value, flags=re.I)
 
     # Extra dashes
-    value = re.sub(r'\-{2,}', '-', value) 
-    value = re.sub(r'(^\-)|(\-$)', '', value) 
+    value = re.sub(r'\-{2,}', '-', value)
+    value = re.sub(r'(^\-)|(\-$)', '', value)
 
     return value
