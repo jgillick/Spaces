7179422ebff8314d35ed200ca5f24e2d2adea6ff
jgillick <j_gillick@yahoo.com>
2015-12-23 22:05:51 -0800

    Customize document form

--- spaces/forms.py
+++ spaces/forms.py
@@ -18,36 +18,45 @@ class DocumentForm(forms.ModelForm):
         user = kwargs.pop('user', None)
         super(DocumentForm, self).__init__(*args, **kwargs)
 
-        # Space list
+        # Existing document
         if self.instance.pk:
             # Disable selecting space if existing document
             self.fields["space"].disabled = True
-        else:
-            # Exclude _ROOT_ and convert _USER_ to username
-            self.fields["space"].queryset = Space.objects.exclude(
-                name=Space.ROOT_SPACE_NAME)
-            self.fields['space'].label_from_instance = lambda obj: \
-                user.username if user and obj.name == Space.USER_SPACE_NAME \
-                else obj.name
 
-        # Set default path
+            # Cannot change path for root document
+            if self.instance.is_space_root:
+                self.fields["path"].disabled = True
+
+        # Exclude _ROOT_ and convert _USER_ to username
+        self.fields["space"].queryset = Space.objects.exclude(
+            name=Space.ROOT_SPACE_NAME)
+        self.fields['space'].label_from_instance = lambda obj: \
+            user.username if user and obj.name == Space.USER_SPACE_NAME \
+            else obj.name
+
+        # Set default path and remove double slashes
         self.initial["path"] = self.instance.full_path(inc_space=False) + "/"
-        # Remove double slashes
         self.initial["path"] = re.sub(r'/+', '/', self.initial["path"])
 
     def clean_path(self):
         doc = None
         path = self.cleaned_data["path"]
 
+        # Empty path
+        if (not path
+            and (not self.instance.pk or not self.instance.is_space_root)):
+            raise ValidationError("A path is required")
+
         # Try to find another document with this pass
-        try:
-            doc = Document.objects.get_by_path(
-                path=path, space=self.instance.space
-            )
-            if not self.instance.pk or doc.pk != self.instance.pk:
-                raise ValidationError("This path already exists")
-        except ObjectDoesNotExist:
-            pass
+        else:
+            try:
+                doc = Document.objects.get_by_path(
+                    path=path, space=self.instance.space
+                )
+                if not self.instance.pk or doc.pk != self.instance.pk:
+                    raise ValidationError("This path already exists")
+            except ObjectDoesNotExist:
+                pass
 
         return path
 
